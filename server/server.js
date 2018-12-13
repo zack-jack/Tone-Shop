b16bb652e685dfda765cf2f3d1001851264351b8
Zack Jackson <z.jackson58@gmail.com>
2018-12-13 11:01:17 -0500

    Set up react client and proxy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(
   process.env.DATABASE,
