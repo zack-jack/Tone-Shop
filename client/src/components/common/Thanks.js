11594e506a3f71eb820f4754e047dcff7ae074b9
Zack Jackson <z.jackson58@gmail.com>
2019-02-15 21:05:29 -0500

    On click handling on thanks page

--- client/src/components/common/Thanks.js
+++ client/src/components/common/Thanks.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Container, Header, Button } from 'semantic-ui-react';
 
-const Thanks = () => {
+const Thanks = props => {
+  const handleOnClick = () => {
+    props.history.push('/account');
+  };
+
   return (
     <Container fluid className="thanks">
       <Header as="h2">Thanks for placing your order!</Header>
       <p>We are working swiftly to process and ship your order.</p>
       <p>You can view the order from your account overview page.</p>
-      <Button as={Link} color="red" to="/account">
+      <Button color="red" onClick={handleOnClick}>
         Go To My Account
       </Button>
     </Container>
